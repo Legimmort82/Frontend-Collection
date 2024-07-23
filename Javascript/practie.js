ef21d93e8378065f827d55a88cd766572d306d19
Legimmort82 <legendlegendery1212@gmail.com>
2024-07-23 11:15:31 +0330

    done

---
+++ Javascript/practie.js
@@ -0,0 +1,39 @@
+//Variables
+
+// a = 3
+// b = "ahmad"
+
+let name = "reza";
+const age = 3.14;
+
+name = "ahmad";
+// age = 32
+
+// type
+let a = 1; // number
+let b = "ali"; // string
+
+// boolean
+let login = true;
+let loginout = false;
+
+let person = { name: "ali", age: 22 }; //object
+let people = [1, 2, 3, 10, 20]; // array
+let hye = undefined;
+let hye1 = null;
+
+// let username = prompt("what is your name?");
+// let userAge = prompt("what is your age?");
+// let userMarried = prompt("Are you married?");
+
+// let userData = { name: username, age: userAge, married: userMarried };
+// console.log(userData);
+let num1 = 23
+let num2 = 26
+let str1 = "ali"
+let str2 = null
+if (a !== 1) {
+  console.log("a = 1");
+} else if (a == 0) {
+  console.log("a = 0");
+} else console.log("a = 1");
