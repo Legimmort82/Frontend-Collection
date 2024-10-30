b6af7ea4462ed8f0a6f3dcfac6cd4e039b66c67f
Legimmort82 <legendlegendery1212@gmail.com>
2024-10-30 11:29:11 +0330

    add test collection to the folder

--- React/Test/Example.test.jsx
+++
@@ -1,7 +0,0 @@
-import Example from "./Example"
-import {render} from "react/testing/library"
-test("Test the title",()=>{
-    render (<Example/>);
-    const title = screen.getByText("Example")
-    expect (title.toBeInTheDocument())
-})
