ef76181a04338e41fda809ad8703b1531dd88e06
Legimmort82 <legendlegendery1212@gmail.com>
2025-07-16 18:02:51 +0330

    basic types Ts

---
+++ TypeScript/main.ts
@@ -0,0 +1,8 @@
+let num: number = 1;
+let text: string = "hello";
+let isMarried: boolean = true;
+let person: { name: string; age: number } = { name: "hi", age: 25 };
+let people: (string | number)[] = [2, "hi", 25];
+let something: any = "hey";
+let Iranian: [string, string, string] = ["Saeed", "Akbar", "Zahra"];
+
